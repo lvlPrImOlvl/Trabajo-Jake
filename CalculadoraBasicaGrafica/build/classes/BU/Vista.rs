4747087efc06807b3aa46ce76868b55a600cfb36
PrImO <baalzephon_evil_raising@hotmail.com>
2015-10-06 14:29:09 -0500

    Calculadora Grafica

---
+++ CalculadoraBasicaGrafica/build/classes/BU/Vista.rs
@@ -0,0 +1,8 @@
+Vista$6
+Vista
+Vista$5
+Vista$4
+Vista$3
+Vista$2
+Vista$1
+Vista$7
---
+++ CalculadoraBasicaGrafica/src/calculadoraBasica.java
@@ -0,0 +1,15 @@
+/**
+ * Esta es la clase principal, donde se manda a llamar todo el programa
+ * @author primo
+ * @version 1.0 06/10/2015
+ */
+public class calculadoraBasica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vista.ejecutar();
+    }
+    
+}
