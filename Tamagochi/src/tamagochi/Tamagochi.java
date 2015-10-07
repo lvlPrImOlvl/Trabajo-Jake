ab0d11d477f97e6c1e2bb1e9e0763d7b78c6db4f
PrImO <baalzephon_evil_raising@hotmail.com>
2015-10-06 22:17:05 -0500

    Tamagochi, dentro en la carpeta dist, viene el jacvadoc

---
+++ Tamagochi/src/tamagochi/Tamagochi.java
@@ -0,0 +1,17 @@
+
+package tamagochi;
+
+/**
+ * @author PrImO
+ * @version 1.0 06/10/2015
+ */
+public class Tamagochi {
+
+    /**
+     * @param args Parametros que se le pasan a main
+     */
+    public static void main(String[] args) {
+        Vista.ejecutar();
+    }
+    
+}
